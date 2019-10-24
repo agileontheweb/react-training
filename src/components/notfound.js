c46958b35d065328eb5a134a1ed3e2a10a0b5f30
Alessandro Cuoghi <agileontheweb@gmail.com>
2019-10-24 16:35:22 +0200

    Add navigation

---
+++ src/components/notfound.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+
+
+const Notfound = () => <h1>Not found</h1>
+export default Notfound
--- src/index.js
+++ src/index.js
@@ -1,10 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import { Route, NavLink, BrowserRouter as Router, Switch } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './App';
+import Users from './users'
+import Contact from './contact'
+import Notfound from './notfound'
+
+import * as serviceWorker from './serviceWorker'
+
+const routing = (
+  <Router>
+    <div>
+      <ul>
+        <li>
+          <NavLink exact activeClassName="active" to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink activeClassName="active" to="/users">Users</NavLink>
+        </li>
+        <li>
+          <NavLink activeClassName="active" to="/contact">Contact</NavLink>
+        </li>
+      </ul>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route exact path="/users/:id" component={Users} />
+        <Route path="/contact" component={Contact} />
+        <Route component={Notfound} />
+      </Switch>
+
+    </div>
+  </Router>
+)
+
+ReactDOM.render(routing,
+                document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
