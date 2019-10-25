dba11d174d39402177b81f269445c895de432270
Alessandro Cuoghi <agileontheweb@gmail.com>
2019-10-25 16:28:21 +0200

    Carousel create Multidimensional aray

---
+++ src/Gallery.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { useState } from 'react';
+
+import {MyCarousel} from './components/MyCarousel';
+
+import Carousel from 'react-bootstrap/Carousel'
+
+
+var galleries = [
+ {
+     id: 1,
+     name: 'Galleria 1',
+     gallery:
+         [{
+           src: 'https://images.pexels.com/photos/2780349/pexels-photo-2780349.jpeg',
+           title: 'titolo 1 immagine'
+         },
+         {
+           src: 'https://images.pexels.com/photos/2858374/pexels-photo-2858374.jpeg',
+           title: 'titolo 2 immagine'
+         }
+       ]
+ },
+ {
+     id: 2,
+     name: 'Galleria 2',
+     gallery:
+         [{
+           src: 'https://images.pexels.com/photos/2780349/pexels-photo-2780349.jpeg',
+           title: 'titolo 1 immagine 2 galleria'
+         },
+         {
+           src: 'https://images.pexels.com/photos/2780349/pexels-photo-2780349.jpeg',
+           title: 'titolo 2 immagine 2 galleria'
+         }
+       ]
+ }
+];
+
+export const Gallery = () => {
+  return(
+    <div>
+      <h1>Gallery</h1>
+
+      {galleries.map((gallery, index) =>
+        <Carousel key={index}>
+          {gallery.gallery.map((gallery, index) =>
+            <Carousel.Item>
+              <img
+                key={index}
+                className="d-block w-100"
+                src={gallery.src}
+                alt={gallery.title}
+              />
+            <Carousel.Caption>
+              <h3>{gallery.title}</h3>
+              <p>text</p>
+            </Carousel.Caption>
+            </Carousel.Item>
+          )}
+        </Carousel>
+      )}
+    </div>
+  )
+}
