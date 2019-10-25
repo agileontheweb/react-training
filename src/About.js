bfe77c42551d9938692855344f5ca5ad3651fd9a
Alessandro Cuoghi <agileontheweb@gmail.com>
2019-10-25 16:26:33 +0200

    Remove carousel from about
    This not work well

--- src/About.js
+++ src/About.js
@@ -2,57 +2,19 @@ import React from 'react';
 import { useState } from 'react';
 
 
-import Carousel from 'react-bootstrap/Carousel'
 import Button from 'react-bootstrap/Button'
 import Modal from 'react-bootstrap/Modal'
-
 export const About = () => {
 
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
-
   return(
     <div>
-      <h1>Hello About</h1>
-      <Carousel>
-        <Carousel.Item>
-          <img
-            className="d-block w-100 h-25"
-            src="https://dummyimage.com/1075x300/000/fff"
-            alt="First slide"
-          />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="https://dummyimage.com/1075x300/000/fff"
-            alt="Third slide"
-          />
+      <h1>About</h1>
 
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="https://dummyimage.com/1075x300/000/fff"
-            alt="Third slide"
-          />
-
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
+      
 
       <Button variant="primary" onClick={handleShow}>
         Launch demo modal
